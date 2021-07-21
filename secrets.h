03e02b9a877d18053e725e3dac37a6644e2faa33
Mehmet-Emre-Dogan <87245315+Mehmet-Emre-Dogan@users.noreply.github.com>
2021-07-22 01:21:12 +0300

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -4,8 +4,8 @@
 /*############################################################################ Gizli Bilgi (Secret Info) ############################################################################*/
 //
 ;
-extern const char* ssid = "yourRouter_sName" // Modem ismi
-extern const char* password = "yourRouter_sPassword" // Modem şifresi
+extern const char* ssid = "yourRouter_sName"; // Modem ismi
+extern const char* password = "yourRouter_sPassword"; // Modem şifresi
 extern const char* deviceName = "Matrix"; // Esp8266 Modem arayüzünde hangi isimle görüntülensin, değiştirebilirsiniz (Esp8266's name on router's user interface, you may change it if you want)                                                                                          
 //
 /*############################################################################ Gizli Bilgi (Secret Info) ############################################################################*/
